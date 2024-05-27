7f431c154942e87c3255956ea002a051cfbf7911
DEEZY1029 <ssobodai@st.ug.edu.gh>
2024-05-27 09:13:00 +0000

    font size changed

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Samuel</Text>
+      <Text style={styles.text}>My name is Samuel</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
      },
-
+text: {
+  fontSize: 24,
+}
 });
